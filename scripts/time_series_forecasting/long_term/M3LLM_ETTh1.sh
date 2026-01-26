b6d05cd3a36ec345bf2a6268a314535c5b38a731
chenyqk <15333175+chenyqk@user.noreply.gitee.com>
2026-01-26 19:56:49 +0800

    update ETTh1 params

--- scripts/time_series_forecasting/long_term/M3LLM_ETTh1.sh
+++ scripts/time_series_forecasting/long_term/M3LLM_ETTh1.sh
@@ -1,9 +1,9 @@
 model_name=M3LLM_Qwen
 mix_embeds=True
 mlp_hidden_layers=0
-multi_scale=True
+multi_scale=False
 multi_var=True
-mix_embeds_type=v1
+mix_embeds_type=v11
 gpu=0
 
 # training one model with a context length
@@ -32,6 +32,10 @@ python -u run.py \
   --mix_embeds $mix_embeds \
   --multi_var $multi_var \
   --multi_scale $multi_scale \
+  --down_sampling_layers 3 \
+  --down_sampling_window 2 \
+  --down_sampling_method avg \
+  --pdm_blocks_num 1 \
   --mix_embeds_type $mix_embeds_type \
   --patience 10 \
   --weight_decay 0.000005 \
@@ -65,9 +69,14 @@ python -u run.py \
   --mix_embeds $mix_embeds \
   --multi_var $multi_var \
   --multi_scale $multi_scale \
+  --down_sampling_layers 3 \
+  --down_sampling_window 2 \
+  --down_sampling_method avg \
+  --pdm_blocks_num 1 \
   --mix_embeds_type $mix_embeds_type \
   --patience 10 \
   --weight_decay 0.000005 \
   --drop_last \
   --test_dir long_term_forecast_ETTh1_672_96_${model_name}_ETTh1_sl672_ll576_tl96_lr0.0009_bt128_wd5e-06_hd256_hl${mlp_hidden_layers}_cosTrue_mix${mix_embeds}_multivar${multi_var}_multiscale${multi_scale}_type${mix_embeds_type}_test_0
 done
+
